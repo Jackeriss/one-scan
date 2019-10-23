defd82da021dca8d159076b73bfdafb833c1d7ca
yufeizhao <yufeizhao@clubfactory.com>
2019-10-23 19:08:12 +0800

    update

--- app/util/struct_util.py
+++ app/util/struct_util.py
@@ -3,9 +3,3 @@ from enum import Enum
 
 class StrEnum(str, Enum):
     """ 字符串类型的枚举 """
-
-    def __new__(cls, *args):
-        for arg in args:
-            if not isinstance(arg, str):
-                raise TypeError("Not str: {}".format(arg))
-        return super(StrEnum, cls).__new__(cls, *args)
